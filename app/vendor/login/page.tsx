158894ad249be75dbde6e606406d8319ccd29b9b
Zion Opaaje <zopaaje8@gmail.com>
2026-08-30 09:15:22 +0100

    Create page.tsx

---
+++ app/vendor/login/page.tsx
@@ -0,0 +1,7 @@
+import { VendorLoginForm } from "./vendor-login-form";
+
+export const metadata = { title: "Vendor login" };
+
+export default function VendorLoginPage() {
+  return <VendorLoginForm />;
+}
